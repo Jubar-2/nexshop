1e9ffe8f27651d9d6a45a6a8740100ab5147907d
Md Jubaer Rahman <mdjubairrahman62782@gmail.com>
2026-07-13 16:11:23 +0600

    add admin

--- app/api/admin/jobs/create/route.ts
+++ app/api/admin/jobs/create/route.ts
@@ -64,7 +64,7 @@ export async function POST(request: Request) {
                 categoryId: category,
                 subCategoryId: subCategory,
                 targetLink:targetLink.trim(),
-                userId: "cmosml12s0003y8qcair8ug4d",//Number(session.user.id),
+                userId: "cmrf45b1g0000y87eia8xj99c",//Number(session.user.id),
             },
         });
 
